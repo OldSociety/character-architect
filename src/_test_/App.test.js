502ed3f3c444a340765e343bcaf0d38384ec9df9
OldSociety <headmistrust@outlook.com>
2022-02-16 10:58:45 -0800

    update clock, set up testing

--- src/_test_/App.test.js
+++ src/_test_/App.test.js
@@ -1,6 +1,4 @@
-import * as React from 'react'
-import { render, screen } from '@testing-library/react';
-import App, {timesThree} from '../App';
+import {timesThree} from '../App';
 
 // NPM TEST files
 
---
+++ src/_test_/Clock.test.js
@@ -0,0 +1,10 @@
+// import calculateTimeLeft from '../Components/Clock/Clock.jsx';
+
+// NPM TEST files
+
+// describe('Clock time', () => {
+//   test('should equal one week from now', () => {
+//     const expected = 6;
+//     expect(calculateTimeLeft).toBe(expected);
+//   });
+// });
