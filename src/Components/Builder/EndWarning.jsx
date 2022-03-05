af4f552f7796b9a23fe8a74a11851565a05205c2
OldSociety <headmistrust@outlook.com>
2022-03-05 11:07:21 -0800

    Stack() for builder with valid to ensure order

--- src/Components/Builder/EndWarning.jsx
+++ src/Components/Builder/EndWarning.jsx
@@ -1,6 +1,11 @@
-import * as React from 'react'
+import * as React from 'react';
+import { userStack, userArray } from 'UserData/User';
 
 const EndForm = () => {
+
+  let data = JSON.stringify(userStack)
+  console.log(data)
+  console.log(typeof data)
   return (
     <div className="container">
       <div className="row g-3">
@@ -8,10 +13,16 @@ const EndForm = () => {
         ton of work to be done before the final version of the website is
         released. Until then, enjoy using the countdown timer at the bottom of the page in anticipation
         our next dnd session.`}
-        <div className="col-12"></div>
+        <div className="row g-3">
+          {/* { data.map((selection, index) => (
+            <ul>
+              <li>{selection}</li>
+            </ul>
+          ))} */}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default EndForm
+export default EndForm;
