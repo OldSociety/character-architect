d8df8d956abe9ea511755d3763b6d468a9c274b0
OldSociety <headmistrust@outlook.com>
2022-03-18 09:40:08 -0700

    heroku test

--- src/Components/Clock/Clock.jsx
+++ src/Components/Clock/Clock.jsx
@@ -1,49 +1,49 @@
 import React, { useState, useEffect } from 'react'
 
 export const calculateTimeLeft = () => {
-  let target = new Date('03/16/22 17:30')
+//   let target = new Date('03/16/22 17:30')
 
-  let difference = + target - +new Date()
+//   let difference = + target - +new Date()
 
-  let timeLeft = {}
+//   let timeLeft = {}
 
-  if (difference > 0) {
-    timeLeft = {
-      d: Math.floor(difference / (1000 * 60 * 60 * 24)),
-      h: Math.floor((difference / (1000 * 60 * 60)) % 24),
-      m: Math.floor((difference / 1000 / 60) % 60),
-      s: Math.floor((difference / 1000) % 60),
-    }
-  }
+//   if (difference > 0) {
+//     timeLeft = {
+//       d: Math.floor(difference / (1000 * 60 * 60 * 24)),
+//       h: Math.floor((difference / (1000 * 60 * 60)) % 24),
+//       m: Math.floor((difference / 1000 / 60) % 60),
+//       s: Math.floor((difference / 1000) % 60),
+//     }
+//   }
 
-  return timeLeft
-}
+//   return timeLeft
+// }
 
-const Clock = () => {
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
+// const Clock = () => {
+//   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setTimeLeft(calculateTimeLeft())
-    }, 1000)
-    return () => clearTimeout(timer)
-  })
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       setTimeLeft(calculateTimeLeft())
+//     }, 1000)
+//     return () => clearTimeout(timer)
+//   })
 
-  const timerComponents = [];
+//   const timerComponents = [];
 
-  Object.keys(timeLeft).forEach((interval) => {
-    if (!timeLeft[interval]) {
-      return;
-    }
+//   Object.keys(timeLeft).forEach((interval) => {
+//     if (!timeLeft[interval]) {
+//       return;
+//     }
   
-    timerComponents.push(
-      <span>
-        {timeLeft[interval]} {interval}{" "}
-      </span>
-    );
-  });
+//     timerComponents.push(
+//       <span>
+//         {timeLeft[interval]} {interval}{" "}
+//       </span>
+//     );
+//   });
 
-  return <div>{timerComponents.length ? timerComponents : <p> {calculateTimeLeft} </p>}</div>
+//   return <div>{timerComponents.length ? timerComponents : <p> {calculateTimeLeft} </p>}</div>
 }
 
 export default Clock;
