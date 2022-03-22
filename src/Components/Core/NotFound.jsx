733b2e33b0fb38dfca230856bfaa0d2991534d8f
OldSociety <headmistrust@outlook.com>
2022-03-22 16:02:26 -0700

    Not found css cleanup

--- src/Components/Core/NotFound.jsx
+++ src/Components/Core/NotFound.jsx
@@ -1,8 +1,12 @@
 export default function NotFound() {
   return (
-    <div className="NotFound">
-      <h4>Error 404</h4>
-      <h1>Page not found</h1>
-    </div>
+    <>
+      <div className="d-flex row main__container min-vh-100">
+        <div className="container col-10 col-lg-8">
+          <h4>Error 404</h4>
+          <h1>Page not found</h1>
+        </div>
+      </div>
+    </>
   );
 }
