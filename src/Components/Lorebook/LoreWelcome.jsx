cc47760c785eb394f625eb32a1494d783cbbb9b4
OldSociety <headmistrust@outlook.com>
2022-03-22 18:33:16 -0700

    Lore setup

--- src/Components/Lorebook/LoreWelcome.jsx
+++ src/Components/Lorebook/LoreWelcome.jsx
@@ -4,12 +4,18 @@ import * as React from 'react';
 // import { useNavigate } from 'react-router-dom';
 
 const LoreWelcome = () => {
-
   return (
     <>
       <div className="d-flex row main__container min-vh-100">
         <div className="container col-10 col-lg-8">
-          <div>Lore</div>
+          <h4>The Land of Meridian</h4>
+          <ul>
+            <li>Heroes</li>
+            <li>Monsters</li>
+            <li>Races</li>
+            <li>Nations</li>
+            <li>Magic versus Technology</li>
+          </ul>
         </div>
       </div>
     </>
