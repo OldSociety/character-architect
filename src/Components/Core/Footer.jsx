df27619b88f0d63efa902d84d0c740ef158db05e
OldSociety <headmistrust@outlook.com>
2022-03-22 18:35:20 -0700

    secret

--- src/Components/Core/Footer.jsx
+++ src/Components/Core/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Clock from '../Clock/Clock';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   const year = new Date().getFullYear();
@@ -100,7 +101,7 @@ const Footer = () => {
                 until the next game
               </li>
               <li className="mb-lg-2 text-light text-end">
-                License © <span>{year}</span>
+                License <Link className="text-decoration-none link-light fw-light" to={"https://support.worldwildlife.org/site/Donation2?df_id=13430&13430.donation=form1&s_src=AWE1800OQ18363A01430RX&msclkid=baf713be356214ea0c5771844c78eb71"}>©</Link><span> {year}</span>
               </li>
               <li className="mb-lg-2 text-light text-end">Currently v0.0.1</li>
             </ul>
