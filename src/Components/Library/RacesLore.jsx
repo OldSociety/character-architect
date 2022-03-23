cc81043e143daa5bdd7bc1ab42006a0c86298f82
OldSociety <headmistrust@outlook.com>
2022-03-23 10:54:54 -0700

    Race display page

---
+++ src/Components/Library/RacesLore.jsx
@@ -0,0 +1,49 @@
+import * as React from 'react'
+import { useState, useEffect } from 'react';
+import { listRaces } from 'utils/api';
+import Cards from 'Components/Cards/Cards'; 
+
+const RacesLore = () => {
+ const [fetchedData, setFetchedData] = useState([])
+ const [raceError, setRaceError] = useState(null)
+//  let { info, results } = fetchedData
+
+useEffect(fetchData, [])
+  
+  function fetchData() {
+    const abortController = new AbortController();
+    setRaceError(null);
+
+    listRaces(abortController.signal).then(setFetchedData).catch(setRaceError);
+    return () => abortController.abort();
+  }
+
+  console.log(fetchedData)
+
+  return (
+    <>
+      <div className="d-flex row main__container min-vh-100">
+        <div className="container col-10 col-lg-8">
+          <nav aria-label="breadcrumb">
+            <ol className="breadcrumb">
+            <li className="breadcrumb-item" aria-current="page">
+                Library
+              </li>
+              <li className="breadcrumb-item active" aria-current="page">
+                Races
+              </li>
+            </ol>
+          </nav>
+          <div className="row">
+          <div className="container">
+          <h4>The Races of Meridian</h4>
+          <Cards results={fetchedData} />
+          </div>
+        </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default RacesLore
