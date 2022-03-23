b44acd00ffc0530dbad9e3ce57872824bdc77228
OldSociety <headmistrust@outlook.com>
2022-03-23 12:50:09 -0700

    Add butons to linked library.

--- src/Components/Library/Library.jsx
+++ src/Components/Library/Library.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 // import { useState, useEffect } from 'react';
 // import { API_BASE_URL, fetchJson } from 'utils/api';
 // import { useNavigate } from 'react-router-dom';
@@ -16,17 +17,34 @@ const LoreWelcome = () => {
             </ol>
           </nav>
           <div className="row">
-          <div className="container">
-          <h4>The Land of Meridian</h4>
-          <ul>
-            <li>Heroes</li>
-            <li>Monsters</li>
-            <li>Races</li>
-            <li>Nations</li>
-            <li>Magic versus Technology</li>
-          </ul>
+            <div className="container">
+              <div className="col d-flex flex-column justify-content-center">
+                <ul>
+                  <button type="button" className="btn btn-outline-secondary fs-5 mb-4">
+                    Heroes
+                  </button>
+                  <br />
+                  <button type="button" className="btn btn-outline-secondary fs-5 mb-4">
+                    Monsters
+                  </button>
+                  <br />
+                  <Link to={'/library/races'}>
+                    <button type="button" className="btn btn-outline-secondary fs-5 mb-4">
+                      Races
+                    </button>
+                    <br />
+                  </Link>
+                  <button type="button" className="btn btn-outline-secondary fs-5 mb-4">
+                    Nations
+                  </button>
+                  <br />
+                  <button type="button" className="btn btn-outline-secondary fs-5 mb-4">
+                    Technology
+                  </button>
+                </ul>
+              </div>
+            </div>
           </div>
-        </div>
         </div>
       </div>
     </>
