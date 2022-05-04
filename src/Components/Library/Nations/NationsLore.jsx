c91534fa369dfef2cc42a46cc5b8ccc7831c0d88
OldSociety <headmistrust@outlook.com>
2022-05-03 18:38:36 -0700

    Meridian map

--- src/Components/Library/Nations/NationsLore.jsx
+++ src/Components/Library/Nations/NationsLore.jsx
@@ -5,6 +5,7 @@ import { listNations } from 'utils/api';
 import Cards from 'Components/Library/Nations/NationCards';
 import ErrorAlert from 'Components/Layout/ErrorAlert';
 
+
 const NationsLore = () => {
   const [fetchedData, setFetchedData] = useState([]);
   const [NationError, setNationError] = useState(null);
@@ -15,7 +16,9 @@ const NationsLore = () => {
     const abortController = new AbortController();
     setNationError(null);
 
-    listNations(abortController.signal).then(setFetchedData).catch(setNationError);
+    listNations(abortController.signal)
+      .then(setFetchedData)
+      .catch(setNationError);
     return () => abortController.abort();
   }
 
@@ -26,7 +29,12 @@ const NationsLore = () => {
           <nav aria-label="breadcrumb">
             <ol className="breadcrumb">
               <li className="breadcrumb-item" aria-current="page">
-                <Link to={`/library`} className="text-decoration-none link-light fw-heavy">Library</Link>
+                <Link
+                  to={`/library`}
+                  className="text-decoration-none link-light fw-heavy"
+                >
+                  Library
+                </Link>
               </li>
               <li className="breadcrumb-item active" aria-current="page">
                 Nations
@@ -40,6 +48,15 @@ const NationsLore = () => {
               <div className="col-12">
                 <div className="row">
                   <Cards page="/" results={fetchedData} />
+                </div>
+                <div className="row">
+                  
+                  <img
+                    className={`map img-fluid`}
+                    src={require(`../../../Images/MeridianMap.png`)}
+                    alt="hello"
+                    width="1000"
+                  />
                 </div>
               </div>
             </div>
