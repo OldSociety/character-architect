93991010e0f1c67f4f5af51b03a90955f0e6dce8
OldSociety <headmistrust@outlook.com>
2022-06-03 11:40:48 -0700

    images to race in builder

--- src/Components/Builder/Races.jsx
+++ src/Components/Builder/Races.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from 'react-router-dom';
 import { API_BASE_URL } from '../../utils/api';
 import { userList } from './Moons';
 
-
 const RaceForm = () => {
   const [formData, setFormData] = useState('default');
   const [fetchedData, updateFetchedData] = useState([]);
@@ -23,69 +22,309 @@ const RaceForm = () => {
 
   // prevent loading out of order
   useEffect(() => {
-    if (userList.moon === "") {
-      navigate(`${baseUrl}`)
+    if (userList.moon === '') {
+      navigate(`${baseUrl}`);
     }
-  })
+  });
 
   console.log('fetched Data', fetchedData);
 
   const handleChange = (event) => {
     setFormData(event.target.value);
-    console.log(userList)
+    console.log(userList);
   };
 
   // Detailed ancestry text
   const handleText = (optionText) => {
     switch (formData) {
       case 'Asha':
-        return `${data[0].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[0].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[0].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'Nyxen':
-        return `${data[1].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[1].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[1].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'Siiq':
-        return `${data[2].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[2].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[2].entries[0].entries[0]}`}</div>
+          </div>
+        );;
       case 'Thesian':
-        return `${data[3].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[3].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[3].entries[0].entries[0]}`}</div>
+          </div>
+        );;
       case 'High':
-        return `${data[4].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[4].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[4].entries[0].entries[0]}`}</div>
+          </div>
+        );;
       case 'Moon':
-        return `${data[5].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[5].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[5].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'Wood':
-        return `${data[6].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[6].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[6].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'Dark':
-        return `${data[7].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[7].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[7].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'Sea':
-        return `${data[8].entries[0].entries[0]} `;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[8].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[8].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'Halfelf':
-        return `${data[9].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[9].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[9].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'Lynx':
-        return `${data[10].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[10].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[10].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'Prideborn':
-        return `${data[11].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[11].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[11].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'Snakeskin':
-        return `${data[12].entries[0].entries[0]}}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[12].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[12].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'Lizardskin':
-        return `${data[13].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[13].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[13].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'Naga':
-        return `${data[14].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[14].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[14].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'Sigri':
-        return `${data[15].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[15].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[15].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'Pureblood':
-        return `${data[16].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[16].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[16].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'Nightblood':
-        return `${data[17].entries[0].entries[0]}}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[17].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[17].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'Wren':
-        return `${data[18].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[18].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[18].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'Blackwren':
-        return `${data[19].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[19].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[19].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'GenerationI':
-        return `${data[20].entries[0].entries[0]}}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[20].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[20].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'GenerationII':
-        return `${data[21].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[21].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[21].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'GenerationIII':
-        return `${data[22].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[22].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[22].entries[0].entries[0]}`}</div>
+          </div>
+        );
       case 'Unmarked':
-        return `${data[23].entries[0].entries[0]}`;
+        return (
+          <div className="">
+            <img
+              className={'1'}
+              src={data[23].entries[1].href.url}
+              alt="missingpicture"
+              width="200"
+            />
+            <div className="">{`${data[23].entries[0].entries[0]}`}</div>
+          </div>
+        );
       default:
         return '';
     }
@@ -94,53 +333,53 @@ const RaceForm = () => {
   const handleUserData = (optionText) => {
     switch (formData) {
       case 'Asha':
-        return `${data[0].name}`;
+        return data[0].name;
       case 'Nyxen':
         return data[1].name;
       case 'Siiq':
         return data[2].name;
       case 'Thesian':
         return data[3].name;
-        case 'High':
-          return `${data[4].name}`;
-        case 'Moon':
-          return `${data[5].name}`;
-        case 'Wood':
-          return `${data[6].name}`;
-        case 'Dark':
-          return `${data[7].name}`;
-        case 'Sea':
-          return `${data[8].name} `;
-        case 'Halfelf':
-          return `${data[9].name}`;
-        case 'Lynx':
-          return `${data[10].name}`;
-        case 'Prideborn':
-          return `${data[11].name}`;
-        case 'Snakeskin':
-          return `${data[12].name}}`;
-        case 'Lizardskin':
-          return `${data[13].name}`;
-        case 'Naga':
-          return `${data[14].name}`;
-        case 'Sigri':
-          return `${data[15].name}`;
-        case 'Pureblood':
-          return `${data[16].name}`;
-        case 'Nightblood':
-          return `${data[17].name}}`;
-        case 'Wren':
-          return `${data[18].name}`;
-        case 'Blackwren':
-          return `${data[19].name}`;
-        case 'GenerationI':
-          return `${data[20].name}}`;
-        case 'GenerationII':
-          return `${data[21].name}`;
-        case 'GenerationIII':
-          return `${data[22].name}`;
-        case 'Unmarked':
-          return `${data[23].name}`;
+      case 'High':
+        return data[4].name;
+      case 'Moon':
+        return `${data[5].name}`;
+      case 'Wood':
+        return `${data[6].name}`;
+      case 'Dark':
+        return `${data[7].name}`;
+      case 'Sea':
+        return `${data[8].name} `;
+      case 'Halfelf':
+        return `${data[9].name}`;
+      case 'Lynx':
+        return `${data[10].name}`;
+      case 'Prideborn':
+        return `${data[11].name}`;
+      case 'Snakeskin':
+        return `${data[12].name}}`;
+      case 'Lizardskin':
+        return `${data[13].name}`;
+      case 'Naga':
+        return `${data[14].name}`;
+      case 'Sigri':
+        return `${data[15].name}`;
+      case 'Pureblood':
+        return `${data[16].name}`;
+      case 'Nightblood':
+        return `${data[17].name}}`;
+      case 'Wren':
+        return `${data[18].name}`;
+      case 'Blackwren':
+        return `${data[19].name}`;
+      case 'GenerationI':
+        return `${data[20].name}}`;
+      case 'GenerationII':
+        return `${data[21].name}`;
+      case 'GenerationIII':
+        return `${data[22].name}`;
+      case 'Unmarked':
+        return `${data[23].name}`;
       default:
         return '';
     }
@@ -218,7 +457,7 @@ const RaceForm = () => {
                 <option value="Wren">Wren</option>
                 <option value="Blackwren">Blackwren</option>
               </optgroup>
-            <optgroup label="Marksmen">
+              <optgroup label="Marksmen">
                 <option value="GenerationI">Generation I</option>
                 <option value="GenerationII">Generation II</option>
                 <option value="GenerationIII">Generation III</option>
